0899e574819f84e62c158f3f82aa1eedb2af6bec
seanDoJo <sddonohoe@msn.com>
2015-06-26 12:05:23 +0200

    more ui changes

--- app/src/main/java/com/cisco/prototype/ledsignaldetection/HomeActivity.java
+++ app/src/main/java/com/cisco/prototype/ledsignaldetection/HomeActivity.java
@@ -1,12 +1,14 @@
 package com.cisco.prototype.ledsignaldetection;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.ActionBarActivity;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 
 
 public class HomeActivity extends Activity {

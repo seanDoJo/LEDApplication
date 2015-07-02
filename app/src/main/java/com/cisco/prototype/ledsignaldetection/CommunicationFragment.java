fd41df50b9dd1c19a9361e9bf9c2189f08eb4303
JessicaLandreth <jessica.landreth@colorado.edu>
2015-07-02 16:23:36 +0200

    Made the world's least exciting hash map

---
+++ app/src/main/java/com/cisco/prototype/ledsignaldetection/CommunicationFragment.java
@@ -0,0 +1,66 @@
+package com.cisco.prototype.ledsignaldetection;
+
+import android.app.Activity;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class CommunicationFragment extends Fragment {
+    private onSendListener mListener;
+
+    public CommunicationFragment(){
+        // This just needs to be here
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_communication, container, false);
+
+        return view;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        /*try {
+            mListener = (onSendListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString() + " must implement onSendListener");
+        }*/
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+    public interface onSendListener{
+        public void onSendListener(String message);
+    }
+
+    public void addMessage(String message) {
+        TextView text = (TextView) getView().findViewById(R.id.text_view);
+        text.append(message);
+    }
+
+}
---
+++ app/src/main/java/com/cisco/prototype/ledsignaldetection/SelectionFragment.java
@@ -0,0 +1,75 @@
+package com.cisco.prototype.ledsignaldetection;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.TextView;
+
+
+import com.cisco.prototype.ledsignaldetection.dummy.DummyContent;
+
+import java.util.ArrayList;
+
+public class SelectionFragment extends Fragment {
+
+    private OnFragmentInteractionListener mListener;
+    private AbsListView mListView;
+    private ArrayAdapter mAdapter;
+    private ArrayList<String> myArray;
+    private AdapterView.OnItemClickListener mMessageHandler = new AdapterView.OnItemClickListener(){
+        public void onItemClick(AdapterView parent, View v, int position, long id){
+            mListener.onFragmentMessage(position);
+        }
+    };
+    public SelectionFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        myArray = new ArrayList<String>();
+        mAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, myArray);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_item, container, false);
+
+        mListView = (AbsListView) view.findViewById(R.id.listView);
+        ((AdapterView<ListAdapter>) mListView).setAdapter(mAdapter);
+
+        mListView.setOnItemClickListener(mMessageHandler);
+        return view;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try{
+            mListener = (OnFragmentInteractionListener)activity;
+        } catch(ClassCastException e){}
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+    public interface OnFragmentInteractionListener {
+        public void onFragmentMessage(int index);
+    }
+
+    public void addDevice(String device){
+        mAdapter.add(device);
+    }
+
+}

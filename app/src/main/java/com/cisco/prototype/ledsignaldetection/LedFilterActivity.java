77ce4321d6314dd037b091f5676c2f8244482cde
seanDoJo <sddonohoe@msn.com>
2015-07-20 11:26:50 +0200

    leds update

--- app/src/main/java/com/cisco/prototype/ledsignaldetection/LedFilterActivity.java
+++ app/src/main/java/com/cisco/prototype/ledsignaldetection/LedFilterActivity.java
@@ -16,9 +16,15 @@ import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Point;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
+import org.opencv.imgproc.Moments;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class LedFilterActivity extends Activity implements CameraBridgeViewBase.CvCameraViewListener2 {
@@ -126,19 +132,46 @@ public class LedFilterActivity extends Activity implements CameraBridgeViewBase.
     private void processMat(){
         Mat hsv = latestMat.clone();
         Mat copy = hsv.clone();
+        List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
+        List<Moments> myMoments = new ArrayList<Moments>();
 
         Imgproc.cvtColor(hsv, hsv, Imgproc.COLOR_RGBA2RGB);
         Imgproc.cvtColor(hsv, hsv, Imgproc.COLOR_RGB2HSV);
 
         Core.inRange(hsv, new Scalar(0, 120, 50), new Scalar(120, 255, 255), hsv);
+        Imgproc.erode(hsv, hsv, sElem);
+        Imgproc.dilate(hsv, hsv, sElem);
+
+        Imgproc.dilate(hsv, hsv, sElem);
+        Imgproc.erode(hsv, hsv, sElem);
         Imgproc.cvtColor(hsv, hsv, Imgproc.COLOR_GRAY2RGBA);
         Core.bitwise_and(hsv, copy, hsv);
 
-        Imgproc.erode(hsv, hsv, sElem);
-        Imgproc.dilate(hsv, hsv, sElem);
+        copy.release();
+        copy = hsv.clone();
+        Imgproc.cvtColor(copy, copy, Imgproc.COLOR_RGBA2GRAY);
 
-        Imgproc.dilate(hsv, hsv, sElem);
-        Imgproc.erode(hsv, hsv, sElem);
+        Imgproc.findContours(copy, contours, new Mat(), Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE, new Point(0, 0));
+        int size = contours.size();
+        for(int i = 0; i < size; i++){
+            myMoments.add(Imgproc.moments(contours.get(i)));
+        }
+        for(Moments moment : myMoments){
+            double dArea = moment.get_m00();
+            if(dArea >= 60 && dArea <= 610) {
+                double dM01 = moment.get_m01();
+                double dM10 = moment.get_m10();
+                int posX = (int) (dM10 / dArea);
+                int posY = (int) (dM01 / dArea);
+                double[] colour = hsv.get(posY, posX);
+                for (int i = 0; i < colour.length; i++) {
+                    System.out.println(Double.toString(colour[i]));
+                }
+                System.out.println(dArea);
+                double radius = Math.sqrt(dArea/3.1415926535);
+                Core.circle(hsv, new Point(posX, posY), (int)radius, new Scalar(255, 255, 255));
+            }
+        }
 
         copy.release();
         latestMat.release();

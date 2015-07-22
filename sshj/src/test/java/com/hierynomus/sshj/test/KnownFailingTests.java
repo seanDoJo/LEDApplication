fcd2e6d01591c7c2b24c9c9f1bc042ae1224e663
seanDoJo <sddonohoe@msn.com>
2015-07-22 17:14:13 +0200

    ssh doesn't work

--- sshj/src/test/java/com/hierynomus/sshj/test/KnownFailingTests.java
+++
@@ -1,9 +0,0 @@
-package com.hierynomus.sshj.test;
-
-/**
- * Marker interface for JUnit Categories.
- *
- * This denotes that the test is known to fail, and should be fixed at some time.
- */
-public interface KnownFailingTests {
-}
